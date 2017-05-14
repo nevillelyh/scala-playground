df5f594f8f26f0b4c7de2676fbe54f3724978d33
Neville Li <neville@spotify.com>
2017-05-14 11:20:59 -0400

    add Cats.Free

--- src/main/scala/Cats.scala
+++ src/main/scala/Cats.scala
@@ -39,6 +39,56 @@ object Cats {
     def followedBy[A, B](fa: F[A])(fb: F[B]): F[B] = flatMap(fa)(_ => fb)
     override def ap[A, B](ff: F[(A) => B])(fa: F[A]): F[B] = flatMap(ff)(f => map(fa)(f))
   }
+
+  sealed trait Free[F[_], A] {
+    def map[B](f: A => B): Free[F, B] = flatMap(a => Pure(f(a)))
+    def flatMap[B](f: A => Free[F, B]): Free[F, B] = FlatMapped(this, f)
+
+    def run[G[_]](transform: F ~> G)(implicit G: Monad[G]): G[A] = this match {
+      case Pure(a) => G.pure(a)
+      case Suspend(a) => transform(a)
+      case FlatMapped(a, f) =>
+        G.flatMap(a.run(transform)) { b =>
+          f(b).run(transform)
+        }
+    }
+  }
+  object Free {
+    def liftM[F[_], A](fa: F[A]): Free[F, A] = Suspend(fa)
+  }
+
+  trait FunctorTransformer[F[A], G[A]] {
+    def apply[A](fa: F[A]): G[A]
+  }
+  type ~>[F[_], G[_]] = FunctorTransformer[F, G]
+
+  case class Pure[F[_], A](a: A) extends Free[F, A]
+  case class Suspend[F[_], A](a: F[A]) extends Free[F, A]
+  case class FlatMapped[F[_], A, B](a: Free[F, A], f: A => Free[F, B]) extends Free[F, B]
+
+  sealed trait DiskIO[A]
+  object DiskIO {
+    case class Read(file: String) extends DiskIO[String]
+    case class Write(file: String, contents: String) extends DiskIO[Boolean]
+    case class Delete(file: String) extends DiskIO[Boolean]
+  }
+
+  val interpreter = new (DiskIO ~> Option) {
+    override def apply[A](fa: DiskIO[A]): Option[A] = fa match {
+      case DiskIO.Read(file) => Some("abc")
+      case DiskIO.Write(file, contents) => Some(true)
+      case DiskIO.Delete(file) => Some(true)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val f = for {
+      a <- Free.liftM(DiskIO.Read("a.txt"))
+      b <- Free.liftM(DiskIO.Write("b.txt", a))
+      c <- Free.liftM(DiskIO.Delete("a.txt"))
+    } yield (a, b, c)
+    println(f.run(interpreter)(Impls.optMonad))
+  }
 }
 
 object Impls {
